76e2864f8233ba1278ce4e6737378eb3efb8a1bb
Mariyan97 <webdev.mvmnt@gmail.com>
2024-04-19 19:43:54 +0300

    Mariyan: I've added the last future of the nav-bar, now it have an animation on every link

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 let lineOne = document.querySelector(".line-1");
 let lineTwo = document.querySelector(".line-2");
 let lineThree = document.querySelector(".line-3");
-
+const navLinks = document.querySelectorAll(".nav-link");
 let burger = document.querySelector(".burger");
 // main-function
 
@@ -12,4 +12,16 @@ burger.addEventListener("click", () => {
   lineOne.classList.toggle("left");
   lineTwo.classList.toggle("opacity");
   lineThree.classList.toggle("right");
+
+  // animate links
+
+  navLinks.forEach((link, index) => {
+    if (link.style.animation) {
+      link.style.animation = "";
+    } else {
+      link.style.animation = `navLinkFade 0.5s ease forwards ${index / 3}s`;
+    }
+  });
 });
+
+// gsap
